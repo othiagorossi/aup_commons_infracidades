4152738a853bb6caaf889eecf231b3b5bb11c7fd
Thiago <tj.angrizanes@gmail.com>
2026-09-21 19:31:30 -0300

    Rename abm_v20_colab.py

--- model/run_abm_protocols.py
+++ model/run_abm_protocols.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 sys.path.insert(0, str(Path(__file__).resolve().parents[1]))
 
-from model.abm_v20_colab import (
+from model.abm_v20 import (
     LongTermUrbanAgricultureModel,
     PolicyConfig,
     ScenarioRunner,
